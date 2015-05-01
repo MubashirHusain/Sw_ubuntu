5460554a835aa869506ef49503ac9f93bab7a298
MubashirHusain <m.hussain@student.unsw.edu.au>
2015-05-01 19:17:30 +1000

    Installing softwares script

---
+++ sw_install_script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#For synaptic-manager installation
+sudo apt-get install synaptic
+
+#For chrome browser installation
+sudo apt-get install chromium-browser
+
+#For skype installation 
+sudo add-apt-repository "deb http://archive.canonical.com/ $(lsb_release -sc) partner"	
+sudo apt-get update
+sudo apt-get install skype
+
+#For installing latex on ubuntu
+sudo apt-get install texlive-full
+sudo apt-get install texmaker
+
+#For installing git
+sudo apt-get install git
+
