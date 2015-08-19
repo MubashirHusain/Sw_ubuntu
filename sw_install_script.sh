6a336b77ff9373b936046e0113df91cd60c36b55
MubashirHusain <mubashir.hussain@live.com>
2015-08-19 21:03:41 +1000

    Installing vim

--- sw_install_script.sh
+++ sw_install_script.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
 #For synaptic-manager installation
-sudo apt-get install synaptic
+sudo apt-get -y install synaptic
 
 #For chrome browser installation
-sudo apt-get install chromium-browser
+sudo apt-get  -y install chromium-browser
 
 #For skype installation 
 sudo add-apt-repository "deb http://archive.canonical.com/ $(lsb_release -sc) partner"	
 sudo apt-get update
-sudo apt-get install skype
+sudo apt-get -y install skype
 
 #For installing latex on ubuntu
-sudo apt-get install texlive-full
-sudo apt-get install texmaker
+#sudo apt-get install texlive-full
+#sudo apt-get install texmaker
 
 #For installing git
-sudo apt-get install git
+sudo apt-get -y install git
+
+#For vim 
+sudo apt-get -y install vim
+
 
